dd564cc850070dab5c108064082841ef1e26fe8a
mustafaKenany <mustafa.kenany2022@gmail.com>
2017-11-30 13:44:48 +0300

    okay

--- database/migrations/2017_11_26_185704_create_employees_table.php
+++ database/migrations/2017_11_26_185704_create_employees_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateStudentsTable extends Migration
+class CreateEmployeesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreateStudentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('students', function (Blueprint $table) {
+        Schema::create('employees', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('studentName');
-            $table->string('image');
+            $table->string('nameEmployee');
+            $table->string('image')->default('noimage.jpg');
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ class CreateStudentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('students');
+        Schema::dropIfExists('employees');
     }
 }
